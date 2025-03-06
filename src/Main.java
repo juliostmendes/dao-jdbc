eddd0b5ac410c42d0891120eb4d636cac950506b
juliostmendes <mendes.julio@pm.me>
2025-03-06 17:10:59 -0300

    FindById impl

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department department = new Department(1, "Human Resources");
-
-        Seller seller = new Seller(1, "João Alves", "joãoalves@udemy.com", new Date(), 2300.0, department);
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
-        System.out.println(department);
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
