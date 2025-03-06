65c4e824587402c2a671384106cde35643a6b602
juliostmendes <mendes.julio@pm.me>
2025-03-06 17:39:25 -0300

    Find by id impl and connection

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        System.out.println("======= TEST 1: Seller findById =========");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
